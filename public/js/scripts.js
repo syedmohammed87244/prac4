6dd6c838c3819c12e6f7f0e2a0434b68828d487f
syedmohammed87244 <syed87244@gmail.com>
2022-11-29 19:22:58 +1100

    prac3

---
+++ public/js/scripts.js
@@ -0,0 +1,50 @@
+const cardList = [
+    {
+        title: "Nissan GTR R35",
+        image: "images/car2.jpg",
+        link: "About Nissan GTR R35",
+        desciption: "Demo desciption about R35"
+    },
+    {
+        title: "Nissan GTR R35 Nissmo",
+        image: "images/car3.jpg",
+        link: "About Nissan GTR R35 Nissmo",
+        desciption: "Demo desciption about R35 Nissmo"
+    }
+]
+const clickMe = () => {
+    alert("Thanks for clicking me. Hope you have a nice day!")
+}
+
+const submitForm = () => {
+    let formData = {};
+    formData.first_name = $('#first_name').val();
+    formData.last_name = $('#last_name').val();
+    formData.password = $('#password').val();
+    formData.email = $('#email').val();
+
+    console.log("Form Data Submitted: ", formData);
+}
+
+const addCards = (items) => {
+    items.forEach(item => {
+        let itemToAppend = '<div class="col s4 center-align">'+
+    '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
+    '</div><div class="card-content">'+
+    '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
+    '<div class="card-reveal">'+
+        '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
+        '<p class="card-text">'+item.desciption+'</p>'+
+      '</div></div></div>';
+      $("#card-section").append(itemToAppend)
+    });
+}
+
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+    $('#formSubmit').click(()=>{
+        submitForm();
+    })
+    addCards(cardList);
+    $('.modal').modal();
+  });
